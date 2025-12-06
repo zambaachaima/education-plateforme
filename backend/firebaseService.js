4a57b78fcb35a2aa4555c40cc28eeecbe770dd57
zambaachaima <zambaachaima917@gmail.com>
2025-12-06 22:33:03 +0100

    Correction

--- backend/firebaseService.js
+++ backend/firebaseService.js
@@ -1,7 +1,12 @@
 import admin from "firebase-admin";
 
 if (!admin.apps.length) {
-  const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+  const decoded = Buffer.from(
+    process.env.FIREBASE_SERVICE_ACCOUNT_BASE64,
+    "base64"
+  ).toString("utf8");
+
+  const serviceAccount = JSON.parse(decoded);
 
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
