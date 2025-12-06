18d9872c86e4e706a3e2b213be43581f015f100c
zambaachaima <zambaachaima917@gmail.com>
2025-12-06 22:22:54 +0100

    Correction

--- backend/firebaseService.js
+++ backend/firebaseService.js
@@ -1,19 +1,8 @@
 import admin from "firebase-admin";
-import fs from "fs";
-import path from "path";
-import { fileURLToPath } from "url";
-
-// Obtenir __dirname en ES modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// Chemin vers le fichier JSON (à adapter selon ton structure)
-const serviceAccountPath = path.join(__dirname, "serviceAccountKey.json");
-
-// Charger le JSON sans import
-const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, "utf8"));
 
 if (!admin.apps.length) {
+  const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
   });
