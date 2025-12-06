fcd40233f9be62407bffc88a6d7ca13f6c7ab700
zambaachaima <zambaachaima917@gmail.com>
2025-12-06 22:12:43 +0100

    Correction

--- backend/firebaseService.js
+++ backend/firebaseService.js
@@ -1,10 +1,19 @@
 import admin from "firebase-admin";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const serviceAccountPath = path.join(__dirname, "serviceAccountKey.json");
+
+const serviceAccount = await import(serviceAccountPath, {
+  assert: { type: "json" }
+});
 
 if (!admin.apps.length) {
-  const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
-
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert(serviceAccount.default),
   });
 }
 
