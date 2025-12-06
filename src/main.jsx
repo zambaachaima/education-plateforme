7bb2408211c16d039f3b849424646aa91ef3b7c8
zambaachaima <zambaachaima917@gmail.com>
2025-12-06 20:53:35 +0100

    Second Commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'; // global base styles
-import './app.css';   // component/page styles
+import './App.css';   // component/page styles
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
